b75ad723f508d2cfd7290ca849021e1b4ef933ad
Stephen Pham <spham@thoughtworks.com>
2017-04-11 15:26:24 +1000

    Add styling to property results shown in rows of list view

--- SearchResults.js
+++ SearchResults.js
@@ -21,13 +21,29 @@ class SearchResults extends Component {
   }
 
   renderRow(rowData, sectionID, rowId) {
+    let price = rowData.price_formatted.split(' ')[0];
     return (
-      <TouchableHighlight underlayColor='#dddddd'>
+      <TouchableHighlight onPress={() => this.rowPressed(rowData.lister_url)}
+        underlayColor='#dddddd'>
         <View>
-          <Text>{rowData.title}</Text>
+          <View style={styles.rowContainer}>
+            <Image style={styles.thumb} source={{ uri: rowData.img_url }} />
+            <View style={styles.textContainer}>
+              <Text style={styles.price}>{price}</Text>
+              <Text style={styles.title} numberOfLines={1}>
+                {rowData.title}
+              </Text>
+            </View>
+          </View>
+          <View style={styles.separator}/>
         </View>
       </TouchableHighlight>
     );
+  }
+
+  rowPressed(listerURL) {
+    console.log('rowPressed: ' + listerURL);
+    let property = this.props.listings.filter(prop => prop.lister_url === listerURL)[0];
   }
 
   render() {
@@ -38,5 +54,33 @@ class SearchResults extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  thumb: {
+    width: 80,
+    height: 80,
+    marginRight: 10
+  },
+  textContainer: {
+    flex: 1
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#dddddd'
+  },
+  price: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#48BBEC'
+  },
+  title: {
+    fontSize: 20,
+    color: '#656565'
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    padding: 10
+  }
+});
 
 module.exports = SearchResults;
